9e704f821aa384ebb346c9c8cbfa5a96d805412e
dieselxxx <danijel.galic@outlook.com>
2022-11-15 13:17:43 +0100

    Created public landing files

---
+++ src/public/console.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types = 1);
+
+/**
+ * Main Documentor console public landing file
+ *
+ * This is a landing file if PHAR archive is accessed
+ * through a command-line.
+ * @since 0.1.0.pre-alpha.M1
+ *
+ * @author Danijel Galić <danijel.galic@outlook.com>
+ * @copyright 2023 FireHub Web Application Framework
+ * @license <https://opensource.org/licenses/OSL-3.0> OSL Open Source License version 3
+ *
+ * @package FireHub\Public
+ *
+ * @version GIT: $Id$ Blob checksum.
+ */
